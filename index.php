3021f95d0e91e37d5ae1bca89aeccd94cd3e96b3
dmeloni <dmeloni@dmeloni>
2013-06-19 17:26:49 +0200

    Archive page correction

--- index.php
+++ index.php
@@ -1,17 +1,31 @@
 <!DOCTYPE html><?php 
 include 'config.php';
 include 'fct/fct_valid.php';
+include 'fct/fct_xsl.php';
+include 'fct/fct_rss.php';
 
 error_reporting(0);
 
 global $DATA_DIR, $CACHE_DIR_NAME, $ARCHIVE_DIR_NAME;
-$indexFile = sprintf('%s/%s/%s', $DATA_DIR, $CACHE_DIR_NAME, 'index.html');
 
 // Autoredirect on boot.php
+$indexFile = sprintf('%s/%s/%s', $DATA_DIR, $CACHE_DIR_NAME, 'index.html');
+
 if(!checkInstall() || !is_file($indexFile)){
 	header('Location: boot.php');
 	return;
 }
 
+
 header('Content-Type: text/html; charset=utf-8');
-readfile($indexFile);
+if(isset($_GET['date'])){
+	$rssFilePath = sprintf('%s/%s/rss_%s.xml', $DATA_DIR, $ARCHIVE_DIR_NAME, $_GET['date']);
+	$rssFile = file_get_contents($rssFilePath);
+	if(is_file($rssFilePath)) {
+		$index = parseXsl('xsl/index.xsl', $rssFile);
+		$index = sanitize_output($index);
+		echo $index;
+	}
+}else{
+	readfile($indexFile);
+}
