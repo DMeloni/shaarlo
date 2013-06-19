ea612d4a95430d021a25ee556781f4e3bfc2d617
dmeloni <dmeloni@dmeloni>
2013-06-19 16:54:09 +0200

    Main Rss correction

--- rss.php
+++ rss.php
@@ -1,6 +1,10 @@
 <?php 
 error_reporting(0);
 header('Content-Type: application/rss+xml; charset=utf-8');
+
+global $DATA_DIR, $CACHE_DIR_NAME, $ARCHIVE_DIR_NAME;
+$indexFile = sprintf('%s/%s/%s', $DATA_DIR, $CACHE_DIR_NAME, 'rss.xml');
+
 $rssFile = 'rss.xml';
 if(is_file($rssFile)){
 	echo file_get_contents($rssFile);
