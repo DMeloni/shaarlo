c24ba38b7c262a8ede3d1de072c153aa0404ab22
dmeloni <dmeloni@dmeloni>
2013-06-20 14:06:47 +0200

    Suppression des doublons lors de la recherche (traduit ça en anglais voir !)

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
-<!DOCTYPE html><?php 
+<?php 
 include 'config.php';
 include 'fct/fct_valid.php';
 include 'fct/fct_xsl.php';
 include 'fct/fct_rss.php';
 
-error_reporting(0);
+// error_reporting(0);
 
 global $DATA_DIR, $CACHE_DIR_NAME, $ARCHIVE_DIR_NAME, $MAX_FOUND_ITEM;
 
@@ -16,44 +16,56 @@ if(!checkInstall() && !is_file($indexFile)){
 	return;
 }
 
-
-header('Content-Type: text/html; charset=utf-8');
-
 if(isset($_GET['q']) && !empty($_GET['q'])){
 	$archiveDir = sprintf('%s/%s', $DATA_DIR, $ARCHIVE_DIR_NAME);
 	$rssFileList = array();
 	$searchTerm = htmlspecialchars($_GET['q']);
 	define('XPATH_RSS_ITEM', '/rss/channel/item');
 	$found = array();
-	if ($handle = opendir($archiveDir)) {
-		while (false !== ($file = readdir($handle))) {
-			if ($file != "." && $file != "..") {
-				sscanf($file, 'rss_%4s%2s%2s.xml', $years, $months, $days);
-				$rssFile = file_get_contents(sprintf('%s/%s', $archiveDir, $file));
-				$xmlContent = getSimpleXMLElement($rssFile);
-				if($xmlContent === false){
-					continue;
-				}
-				$rssFileArrayed = convertXmlToTableau($xmlContent, XPATH_RSS_ITEM);
-				$nbFoundItems = 0;
-				foreach($rssFileArrayed as $item){
-					if((strpos(strtolower($item['description']),strtolower($searchTerm))!==false)
-					|| (strpos(strtolower($item['link']),strtolower($searchTerm))!==false)
-					|| (strpos(strtolower($item['title']),strtolower($searchTerm))!==false)
-					|| (strpos(strtolower($item['category']),strtolower($searchTerm))!==false)
+	$linkAlreadyFound = array();
+	$nbFoundItems = 0;
+	$fileList =	scandir($archiveDir, 0);
+	foreach ($fileList as $file ) {
+		if ($file != "." && $file != "..") {
+			sscanf($file, 'rss_%4s%2s%2s.xml', $years, $months, $days);
+			$rssFile = file_get_contents(sprintf('%s/%s', $archiveDir, $file));
+			$xmlContent = getSimpleXMLElement($rssFile);
+			if($xmlContent === false){
+				continue;
+			}
+			$rssFileArrayed = convertXmlToTableau($xmlContent, XPATH_RSS_ITEM);
+			foreach($rssFileArrayed as $item){
+				if((strpos(strtolower($item['description']),strtolower($searchTerm))!==false)
+				|| (strpos(strtolower($item['link']),strtolower($searchTerm))!==false)
+				|| (strpos(strtolower($item['title']),strtolower($searchTerm))!==false)
+				|| (strpos(strtolower($item['category']),strtolower($searchTerm))!==false)
+				){
+					if(!array_key_exists($item['link'], $linkAlreadyFound) 
+					|| $linkAlreadyFound[$item['link']] < strlen($item['description'])
 					){
-						$found[] = $item;
+						$linkAlreadyFound[$item['link']] = strlen($item['description']); 
+						$found[$item['link']] = $item;
 						$nbFoundItems++;
 					}
 				}
-				if($nbFoundItems >= $MAX_FOUND_ITEM){
-					break;
-				}
+			}
+			if($nbFoundItems >= $MAX_FOUND_ITEM){
+				break;
 			}
 		}
-		closedir($handle);
 	}
-	rsort($found);
+	
+	// Obtient une liste de colonnes
+	$dateToSort = array();
+	foreach ($found as $key => $row) {
+		$dateToSort[$key]  = $row['pubDate'];
+	}
+	
+	// Trie les données par volume décroissant, edition croissant
+	// Ajoute $data en tant que dernier paramètre, pour trier par la clé commune
+	if(count($dateToSort) === count($found)){
+		array_multisort($dateToSort, SORT_DESC, $found);
+	}
 	
 	$shaarloRss = '<?xml version="1.0" encoding="utf-8"?>
 	<rss version="2.0" xmlns:content="http://purl.org/rss/1.0/modules/content/">
@@ -71,26 +83,42 @@ if(isset($_GET['q']) && !empty($_GET['q'])){
 								<description>%s</description>
 								<category>%s</category>
 								</item>",
-				htmlspecialchars($item['title']), htmlspecialchars($item['link']), $item['pubDate'],htmlspecialchars($item['description']), $item['category']
+				htmlspecialchars($item['title']), 
+				htmlspecialchars($item['link']), 
+				$item['pubDate'],
+				htmlspecialchars($item['description']), 
+				$item['category']
 		);	
 	}
 	$shaarloRss .= '</channel></rss>';
 	
-	$index = parseXsl('xsl/index.xsl', $shaarloRss, array('searchTerm' => $_GET['q']));
-	$index = sanitize_output($index);
-	echo $index;
-		
+	if(isset($_GET['do']) && $_GET['do'] === 'rss'){
+		header('Content-Type: application/rss+xml; charset=utf-8');
+		echo sanitize_output($shaarloRss);
+	}else{
+		$index = parseXsl('xsl/index.xsl', $shaarloRss, array('searchTerm' => $_GET['q']));
+		$index = sanitize_output($index);
+		header('Content-Type: text/html; charset=utf-8');
+		echo $index;
+	}
 }else{
+	?><!DOCTYPE html><?php
 	if(isset($_GET['date'])){
 		$rssFilePath = sprintf('%s/%s/rss_%s.xml', $DATA_DIR, $ARCHIVE_DIR_NAME, $_GET['date']);
 		$rssFile = file_get_contents($rssFilePath);
 		if(is_file($rssFilePath)) {
 			$index = parseXsl('xsl/index.xsl', $rssFile);
 			$index = sanitize_output($index);
+			header('Content-Type: text/html; charset=utf-8');
 			echo $index;
 		}
 	}else{
-		readfile($indexFile);
+		if(is_file($indexFile)){
+			header('Content-Type: text/html; charset=utf-8');
+			readfile($indexFile);
+		}else{
+			header('Location: refresh.php?oneshoot=true');
+		}
 	}
 }
 
