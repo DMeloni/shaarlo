e128a15104f84d13ddc8b3cc4f3fdac9723714b3
DMeloni <denismeloni@gmail.com>
2014-05-24 15:06:59 +0200

    Activation du login_secure par défaut

--- mod/mod.php
+++ mod/mod.php
@@ -3,7 +3,7 @@
 $GLOBALS['MOD'] = array();
 $GLOBALS['MOD']['admin.php_top'] = $GLOBALS['MOD']['admin.php_bottom'] = $GLOBALS['MOD']['index.php_top'] = $GLOBALS['MOD']['index.php_bottom'] = '';
 
-//require_once 'login_secure/login_secure.php';
+require_once 'login_secure/login_secure.php';
 //require_once 'reload_info/reload_info.php';
 //require_once 'tags_info/tags_info.php';
 //require_once 'proposition/proposition.php';
